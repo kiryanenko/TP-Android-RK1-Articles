90ecfd0bb5a6fbcc3616f697aa11cfa8c6d6a8a2
Alexander <kiryanenkoav@mail.ru>
2018-03-07 20:11:32 +0300

    Add lib

--- app/src/main/java/ru/mail/park/rk1_articles/MainActivity.java
+++ app/src/main/java/ru/mail/park/rk1_articles/MainActivity.java
@@ -8,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import ru.mail.park.articlelistlib.Article;
+import ru.mail.park.articlelistlib.ArticleListFragment;
+import ru.mail.park.articlelistlib.OnArticleClickListener;
+
+public class MainActivity extends AppCompatActivity implements OnArticleClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +47,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onArticleClick(Article article) {
+
+    }
 }
