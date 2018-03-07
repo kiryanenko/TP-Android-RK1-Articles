4f8df7ad32c2877a8d80ace125b29205bab718bb
Alexander <kiryanenkoav@mail.ru>
2018-03-07 19:11:11 +0300

    Set orientation

--- app/src/main/java/ru/mail/park/rk1_articles/MainActivity.java
+++ app/src/main/java/ru/mail/park/rk1_articles/MainActivity.java
@@ -1,9 +1,12 @@
 package ru.mail.park.rk1_articles;
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-        transaction.replace(R.id.article_container, new ArticleFragment());
-        transaction.replace(R.id.article_list_container, new ArticleListFragment());
+        Fragment main = getSupportFragmentManager().findFragmentById(R.id.main_container);
+        Fragment article = getSupportFragmentManager().findFragmentById(R.id.article_container);
+        Fragment articleList = getSupportFragmentManager().findFragmentById(R.id.article_list_container);
+
+        int orientation = getResources().getConfiguration().orientation;
+        Log.i("orientation", Integer.valueOf(orientation).toString());
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (article != null && article.isAdded()) {
+                transaction.remove(article);
+            }
+            if (articleList != null && articleList.isAdded()) {
+                transaction.remove(articleList);
+            }
+            transaction.replace(R.id.main_container, new ArticleListFragment());
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (main != null && main.isAdded()) {
+                transaction.remove(main);
+            }
+            transaction.replace(R.id.article_container, new ArticleFragment());
+            transaction.replace(R.id.article_list_container, new ArticleListFragment());
+        }
         transaction.commit();
     }
 
